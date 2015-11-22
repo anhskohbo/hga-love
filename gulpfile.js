a57e1abf346f336c174b379c476231a95da756e1
Nguyễn Văn Ánh <anhskohbo@gmail.com>
2015-11-22 15:16:43 +0700

    Simple code

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 
-elixir(function(mix) {
-    mix.less('main.less');
-    mix.copy(mix.bowerDir+'/bootstrap/fonts', './fonts');
+var notify = require('gulp-notify');
+var handleErrors = function(errorObject, callback) {
+  notify.onError(errorObject.toString()).apply(this, arguments);
+  if (typeof this.emit === 'function') this.emit('end');
+};
 
-    mix.scripts([
-        'jquery/dist/jquery.js',
-        'timecircles/inc/TimeCircles.js',
-        'fullpage.js/jquery.fullPage.js',
-    ], './js/vendor.js', mix.bowerDir);
+gulp.task('sass', function () {
+  return gulp.src('sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass()).on('error', handleErrors)
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({ stream: true, match: '**/*.css' }));
 });
+
+gulp.task('watch', function() {
+  browserSync.init({
+    files: ['*.html'],
+    server: { baseDir: './' },
+  });
+
+  gulp.watch(['sass/**/*.scss'], ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
--- js/main.js
+++ js/main.js
@@ -1,48 +1,50 @@
-$(function() {
-    $('#main').hide().removeClass('hidden');
+(function($) {
+  'use strict';
 
-    $('#main').fullpage({
-        scrollBar: true,
-        navigation: false,
-        anchors: ['about', 'love-days'],
-        sectionsColor: ['#ED5565', '#1BBC9B'],
-    });
+  /**
+   * Main Page.
+   */
+  $(function() {
+    var $main = $('#main');
+    $main.hide().removeClass('hidden');
 
-    $('.avatar > img').click(function() {
-        var el = $(this);
-
-        $(this).removeClass('avatar-box-shadow');
-        $(this).removeClass('shadow').addClass('shadow');
-
-        el.before(el.clone(true)).remove();
-    });
-
-    $('.avatar > img').on('animationend', function() {
-        // $('.avatar > img').addClass('avatar-box-shadow');
+    $main.fullpage({
+      scrollBar: true,
+      navigation: false,
+      anchors: ['about', 'love-days'],
+      sectionsColor: ['#ED5565', '#1BBC9B'],
     });
 
     setTimeout(function() {
-        $('#main').fadeIn();
-        $('#loading').addClass('hidden');
+      $main.fadeIn();
+      $('#loading').addClass('hidden');
 
-        $('.countdown-time').TimeCircles({
-            fg_width: 0.005,
-            bg_width: 0.2,
-            circle_bg_color: '#D8F1F1',
-            time: {
-                Days: {
-                    color: '#fff'
-                },
-                Hours: {
-                    color: '#fff'
-                },
-                Minutes: {
-                    color: '#fff'
-                },
-                Seconds: {
-                    color: '#fff'
-                }
-            }
-        });
+      $('.countdown-time').TimeCircles({
+        bg_width: 0.2,
+        fg_width: 0.005,
+        circle_bg_color: '#D8F1F1',
+        time: {
+          Days: { color: '#fff'  },
+          Hours: {  color: '#fff' },
+          Minutes: { color: '#fff' },
+          Seconds: { color: '#fff' }
+        }
+      });
     }, 1500);
-});
+  });
+
+  /**
+   * Avatar Animation.
+   */
+  $(function() {
+    $('.avatar > img').click(function() {
+      var el = $(this);
+
+      $(this).removeClass('avatar-box-shadow');
+      $(this).removeClass('shadow').addClass('shadow');
+
+      el.before(el.clone(true)).remove();
+    });
+  });
+
+})(jQuery);
